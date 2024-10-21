31aeb92c70135a3c1ae8a4446569cc6d650c407e
Daniel Kimutai <danielkimutaikorir@gmail.com>
2024-10-21 16:53:50 +0300

    q

---
+++ dag.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator  # Updated import for newer versions
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 10, 21),
+    'depends_on_past': False,
+    'email': ['danielkyme@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG('fetch_cricket_stats',
+          default_args=default_args,
+          description='Runs a Python script to fetch cricket statistics',
+          schedule_interval='@daily',
+          catchup=False)
+
+with dag:
+    run_script_task = BashOperator(
+        task_id='run_script',
+        bash_command='python3 /home/kokim/projects/cricket-statistics/extract_data.py',  # Explicitly using python3
+    )
---
+++ trigger_df_job.py
@@ -0,0 +1,29 @@
+from googleapiclient.discovery import build
+import base64
+import google.auth
+import os
+
+def hello_pubsub():   
+ 
+    service = build('dataflow', 'v1b3')
+    project = "lithe-catbird-434312-p9"
+
+    template_path = "gs://dataflow-templates-us-central1/latest/GCS_Text_to_BigQuery"
+
+    template_body = {
+        "jobName": "cricket_dataflow_qa",  # Provide a unique name for the job
+        "parameters": {
+        "javascriptTextTransformGcsPath": "gs://cricket_metadata11112/udf.js",
+        "JSONPath": "gs://cricket_metadata11112/bq.json",
+        "javascriptTextTransformFunctionName": "transform",
+        "outputTable": "lithe-catbird-434312-p9:crickt_dataset.batsman_ranking",
+        "inputFilePattern": "gs://cricket_data_korir/batsmen_rankings.csv",
+        "bigQueryLoadingTemporaryDirectory": "gs://cricket_metadata11112",
+        }
+    }
+
+    request = service.projects().templates().launch(projectId=project,gcsPath=template_path, body=template_body)
+    response = request.execute()
+    print(response)
+
+hello_pubsub()
